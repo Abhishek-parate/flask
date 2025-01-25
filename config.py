577e76528987eb38941eb78883f83173753ba5d4
Abhishek-parate <abhishek.parate.mcm@ghrccst.raisoni.net>
2025-01-25 20:52:21 +0530

    Remove unused type hinting and package metadata files

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'mail.techinbox.in'
+    MAIL_SERVER = ''
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'ceo@techinbox.in'
-    MAIL_PASSWORD = 'Abhi@9860'
-    MAIL_DEFAULT_SENDER= 'ceo@techinbox.in'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER= ''
